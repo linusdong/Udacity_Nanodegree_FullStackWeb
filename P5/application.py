70a24e71df19b30e7c6bb30305a2e32ba6fdefc8
Linus Dong <linusdong@gmail.com>
2015-05-15 21:43:33 -0700

    RFT port to 80

--- P5/application.py
+++ P5/application.py
@@ -442,4 +442,4 @@ def disconnect():
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=80)
